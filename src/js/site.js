5ce9b76136c0089c1f58d0a75696cb73a5d288d9
Lael Birch <lael@ideas42ventures.com>
2021-07-27 09:53:36 -0400

    Change buttondown to mailchimp for email signup forms (#113)

--- src/js/site.js
+++ src/js/site.js
@@ -19,66 +19,6 @@ function closeMobileMenu() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  function showFormSuccess(form, msgEl, text) {
-    form.reset();
-    msgEl.innerHTML = text;
-    msgEl.classList.remove("hidden");
-  }
-
-  function showFormError(msgEl, text) {
-    msgEl.innerHTML = text;
-    msgEl.classList.remove("hidden");
-  }
-
-  function toggleFormBtn(btn, state = "on") {
-    if (state === "off") {
-      btn.classList.add("btn--disabled");
-      btn.innerText = "Subscribing....";
-      return;
-    }
-    btn.classList.remove("btn--disabled");
-    btn.innerText = "Subscribe";
-  }
-
-  [...document.querySelectorAll(".newsletter-form")].forEach((form) => {
-    const msgEl = form.querySelector(".form__msg");
-    const btn = form.querySelector("button[type=submit]");
-
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const url = form.getAttribute("action");
-      const body = new URLSearchParams(new FormData(form)).toString();
-
-      toggleFormBtn(btn, "off");
-
-      fetch(url, {
-        method: "post",
-        body,
-      })
-        .then((res) => {
-          toggleFormBtn(btn);
-          return res.json();
-        })
-        .then((data) => {
-          if (data.statusCode === 400) {
-            throw new Error(data.message);
-          }
-          showFormSuccess(form, msgEl, data.message);
-          return;
-        })
-        .catch((err) => {
-          if (err instanceof SyntaxError) {
-            showFormError(
-              msgEl,
-              "Sorry, something went wrong on our end. Please try again"
-            );
-          } else {
-            showFormError(msgEl, err);
-          }
-        });
-    });
-  });
-
   try {
     [...document.querySelectorAll("a[data-fathom-goal]")].forEach((a) => {
       a.addEventListener("click", (e) => {
